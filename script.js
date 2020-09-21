3c19043cf6659e74731160b0f4d4b207641bf132
drewatienza <atienza.drew@gmail.com>
2020-09-20 20:44:39 -0700

    populate ui with saved data and made final styling adjustments

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
 // Save selected movie index and price
 const setMovieData = (movieIndex, moviePrice) => {
-  localStorage.setItem("selectedMoveIndex", movieIndex);
+  localStorage.setItem("selectedMovieIndex", movieIndex);
   localStorage.setItem("selectedMoviePrice", moviePrice);
 };
 
@@ -25,6 +27,25 @@ const updateSelectedCount = () => {
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 };
+
+// Get data from localStorage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
 
 // Movie select event
 movieSelect.addEventListener("change", (e) => {
@@ -44,3 +65,6 @@ container.addEventListener("click", (e) => {
     updateSelectedCount();
   }
 });
+
+// Initial count and total set
+updateSelectedCount();
